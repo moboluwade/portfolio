f9c1024c34d7319aac1b5adc2d3eb3562e204620
moboluwade <folarinboluwade.com>
2023-12-27 15:48:35 +0100

    style(landing): center profile image layout on small screens

--- src/app/components/Landing/Landing.tsx
+++ src/app/components/Landing/Landing.tsx
@@ -21,10 +21,10 @@ const Landing = () => {
   }, [toggleTo])
 
   return (
-    <div className='px-4 pt-28 md:px-12 flex flex-col justify-center z-10'>
-      <div className=' flex flex-col md:flex-row justify-center gap-12 md:px-8 lg:px-32'>
-        <div className='flex flex-col justify-center'>
-          <div className=' w-[20rem] h-[20rem] rounded-full bg-[#444] shadow-2xl opacity-80 flex flex-row justify-center content-center'>
+    <div className='px-4 pt-10 md:pt-28 md:px-12 flex flex-col justify-center z-10'>
+      <div className=' flex flex-col md:flex-row justify-center gap-12 md:px-8 lg:px-32 w-full'>
+        <div className='flex flex-col justify-center content-center'>
+          <div className='m-auto w-[20rem] h-[20rem] rounded-full bg-[#444] shadow-2xl opacity-80 flex flex-row justify-center content-center'>
             <Image className='rounded-full m-auto md:hidden' src="/profile.jpg" width={250} height={250} alt="mob's profile" />
             <Image className='rounded-full m-auto hidden md:block' src="/profile.jpg" width={300} height={300} alt="mob's profile" />
           </div>
@@ -38,7 +38,6 @@ const Landing = () => {
                 initial={{ opacity: 0, y: 10 }}
                 animate={{ opacity: 100, y: 0 }}
                 exit={{ opacity: 0, y: -10 }}
-                
                 className=''>
                 <Image className={`rounded-sm ${toggleTo !== 1 ? 'hidden' : 'inline-block'}`} src="/ts-logo-128.png" width={24} height={24} alt='ts-logo' />
               </motion.div>
@@ -48,8 +47,8 @@ const Landing = () => {
             </div>
             <h4 className='font-normal md:text-xl text-white opacity-50 inline-block'>Web Developer</h4>
           </div>
-          <h2 className='md:text-5xl font-bold'>Moboluwade Folarin</h2>
-          <h4 className='md:text-lg xl:max-w-[40rem]'>Hi, I am a builder dedicated to bringing scalable and next-level web applications to life with music, with words, and with code.</h4>
+          <h2 className='md:text-5xl text-5xl font-bold'>Moboluwade Folarin</h2>
+          <h4 className='md:text-lg xl:max-w-[40rem] max-w-[90%]'>Hi, I am a builder dedicated to bringing scalable and next-level web applications to life with music, with words, and with code.</h4>
           {/* <h3>Let`&apos;`s Build!</h3> */}
           <div className='flex flex-row gap-4 w-fit'>
             <button className='border-[3px] border-blue-500 rounded-3xl px-6 py-2'>Portfolio</button>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Landing } from "./components/Landing";
+import { Portfolio } from "./components/Portfolio";
 
 export default function Home() {
   return (
         <main className="h-96 pt-10">
           <Landing />
+          <Portfolio />
         </main>
   )
 }
