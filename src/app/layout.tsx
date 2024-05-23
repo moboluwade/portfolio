3861f44cb5e7eea916aa29e6ef86b235d34b56f2
moboluwade <folarinboluwade.com>
2024-05-23 02:55:53 +0100

    feat(font): switch font from Inter to OpenSans

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,16 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+// import { Inter } from 'next/font/google'
+import { Open_Sans } from 'next/font/google'
 import ThemeContext from './ThemeContext'
 import './globals.css'
 import { Header } from './components/Header'
 import { Footer } from './components/Footer'
-const inter = Inter({ subsets: ['latin'] })
+
+const inter = Open_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'mob_codes',
-  description: 'Hi there, I am moboluwade a javascript and typescript web developer, excited about building real-world solutions.',
+  description: 'Hi there, I am Moboluwade a javascript and typescript web developer, excited about building real-world solutions.',
 }
 
 export default function RootLayout({
