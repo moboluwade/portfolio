693d64db67a30b02a334106908df95a88fc38433
moboluwade <folarinboluwade.com>
2024-06-19 15:41:06 +0100

    feat: revamp portfolio header

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -10,6 +10,7 @@ import LinkedinSVG from '@/app/_lib/linkedin-svg'
 
 const Header = () => {
     const [showSidebar, setShowSidebar] = useState(false)
+    const [openSocials, setOpenSocials] = useState(false) // this toggle opens up social links in the header.
     const goToPortfolio = () => {
         const portfolioSection = document.getElementById("portfolio")
         if (!!portfolioSection) {
@@ -17,7 +18,9 @@ const Header = () => {
         }
     }
     return (
-        <div className="w-full fixed overflow-hidden z-20 pt-2 lg:pt dark:bg-[#00000000] backdrop-blur-sm">
+        <div
+            onMouseLeave={() => { setOpenSocials(false), console.log(openSocials) }}
+            className="w-full fixed z-20 pt-2 lg:pt dark:bg-[#00000000] backdrop-blur-sm">
 
             <div className="relative w-full px-4 md:px-12 lg:px-24 md:pt-4">
                 <div className=" flex flex-row relative justify-between items-start">
@@ -26,8 +29,11 @@ const Header = () => {
                     <Image className="dark:inline-block hidden w-10" width={150} height={100} src="https://utfs.io/f/480f992a-39f3-479d-ac45-902412bcb5b2-7sax6c.svg" alt="logo" />
 
                     {/* sub menu list */}
-                    <div className="flex flex-col justify-start gap-4">
+                    <div className="flex flex-col justify-start gap-4 relative">
                         <div className="flex-row gap-7 w-fit flex relative -top-1">
+                            <div className="absolute -left-16 -top-1">
+                                <ThemeSwitcher />
+                            </div>
                             <Link href="#portfolio" onClick={goToPortfolio} scroll={false}>
                                 <span className="font-semibold">Home</span>
                             </Link>
@@ -41,14 +47,16 @@ const Header = () => {
                                 <span className=" font-semibold opacity-50">Blog</span>
                             </Link>
                         </div>
-                        <div className="flex flex-row justify-between items-center">
-                            <div className='h-11 w-44 rounded-full flex flex-row px-4 justify-between items-center bg-[rgba(0,0,0,0.2)] dark:bg-[rgba(255,255,255,0.2)]'>
+                        <div
+                            className="flex flex-row w-full justify-between items-center absolute -bottom-12 right-0">
 
+                            <div className={`h-11 w-44 rounded-full flex flex-row px-4  ${openSocials ? `inline-block` : `invisible`} justify-between items-center bg-[rgba(0,0,0,0.2)] dark:bg-[rgba(255,255,255,0.2)]`}>
                                 <Link target="_blank" href="https://twitter.com/moboluwade_"><XSVG /></Link>
                                 <Link href="https://www.github.com/moboluwade"><GithubSVG /></Link>
                                 <Link target="_blank" href="https://www.linkedin.com/in/boluwade-folarin/"><LinkedinSVG /></Link>
                             </div>
-                            <div>
+
+                            <div onMouseEnter={() => { setOpenSocials(true) }}>
                                 <Link href="https://www.github.com/moboluwade"><GithubSVG /></Link>
                             </div>
                         </div>
@@ -58,7 +66,6 @@ const Header = () => {
                 </div>
             </div>
             <div className="flex flex-row justify-center">
-                <ThemeSwitcher />
             </div>
         </div>
     )
